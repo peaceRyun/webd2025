6d6d23866fb8e646de8ac1ee3f3e0ba01581f2f9
peaceRyun <dbsrn1110@gmail.com>
2025-05-30 22:36:31 +0900

    3

--- siteA-1/script/script.js
+++ siteA-1/script/script.js
@@ -1,4 +1,5 @@
 window.onload = () => {
+    //gnb
     const navList = document.querySelector('.nav ul');
     navList.addEventListener('mouseover', () => {
         document.querySelectorAll('.submenu').forEach((sub) => {
@@ -11,6 +12,7 @@ window.onload = () => {
         });
     });
 
+    //슬라이더
     let currentIndex = 0; //현재 이미지 설정
     const sliderWrap = document.querySelector('.sliderWrap'); //전체 이미지
     const slider = document.querySelectorAll('.slider'); //각각의 이미지
@@ -32,4 +34,32 @@ window.onload = () => {
             }, 700);
         }
     }, 3000);
+
+    //팝업
+    const btnOpen = document.querySelector('.btn_open');
+    const btnClose = document.querySelector('.btn_close');
+    btnOpen.addEventListener('click', () => {
+        document.querySelector('.tab_pop').classList.add('on');
+    });
+    btnClose.addEventListener('click', (e) => {
+        e.preventDefault;
+        document.querySelector('.tab_pop').classList.remove('on');
+    });
+
+    //탭
+    let tabs = document.querySelectorAll('.tabs > a');
+    let tab_contents = document.querySelectorAll('.tab_contents > ul');
+    tabs.forEach(function (tab, index) {
+        tab.addEventListener('click', (e) => {
+            e.preventDefault();
+            //탭기능 활성화
+            tabs[0].classList.remove('on');
+            tabs[1].classList.remove('on');
+            tab.classList.add('on');
+            //탭내용변경
+            tab_contents[0].classList.remove('on');
+            tab_contents[1].classList.remove('on');
+            tab_contents[index].classList.add('on');
+        });
+    });
 };
