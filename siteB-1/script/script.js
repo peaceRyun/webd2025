9e176cbd10a38bd263bb9ab82f1400097a1c69e1
peaceRyun <dbsrn1110@gmail.com>
2025-05-31 12:36:46 +0900

    5

--- siteB-1/script/script.js
+++ siteB-1/script/script.js
@@ -15,17 +15,19 @@ window.onload = () => {
     let currentIndex = 0;
     const sliderWrap = document.querySelector('.slider_wrap');
     const slider = document.querySelectorAll('.slider');
-    // const firstClone = firstElement.clone();
+    const firstClone = sliderWrap.firstElementChild.cloneNode(true);
+    sliderWrap.appendChild(firstClone);
 
     setInterval(() => {
         currentIndex++;
-        sliderWrap.style.transition = 'all 0.3s';
+        sliderWrap.style.transition = 'all 0.6s';
         sliderWrap.style.marginTop = -currentIndex * 300 + 'px';
+        if (currentIndex == slider.length) {
+            setTimeout(() => {
+                sliderWrap.style.transition = '0s';
+                sliderWrap.style.marginTop = '0';
+                currentIndex = 0;
+            }, 700);
+        }
     }, 3000);
-    if (slider.length == 3) {
-        setTimeout(() => {
-            sliderWrap.style.transition = '0';
-            currentIndex = 0;
-        }, 700);
-    }
 };
