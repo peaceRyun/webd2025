813ab059494ac491e4d334fc3b1d349188746822
peaceRyun <dbsrn1110@gmail.com>
2025-05-31 15:36:47 +0900

    6

--- siteB-1/script/script.js
+++ siteB-1/script/script.js
@@ -11,6 +11,7 @@ window.onload = () => {
             sub.style.height = '0';
         });
     });
+
     // sliders
     let currentIndex = 0;
     const sliderWrap = document.querySelector('.slider_wrap');
@@ -30,4 +31,39 @@ window.onload = () => {
             }, 700);
         }
     }, 3000);
+
+    //pop
+    const popOpen = document.querySelector('.pop_open');
+    const popClose = document.querySelector('.pop_close');
+    popOpen.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.querySelector('.pop_wrap').classList.add('on');
+    });
+    popClose.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.querySelector('.pop_wrap').classList.remove('on');
+    });
+
+    //tab
+    const noticeBtn = document.querySelector('.notice');
+    const galleryBtn = document.querySelector('.gallery');
+    const noticeCont = document.querySelector('.notice_cont');
+    const galleryCont = document.querySelector('.gallery_cont');
+
+    noticeBtn.addEventListener('click', () => {
+        noticeBtn.classList.remove('on');
+        galleryBtn.classList.remove('on');
+        noticeCont.classList.remove('on');
+        galleryCont.classList.remove('on');
+        noticeBtn.classList.add('on');
+        noticeCont.classList.add('on');
+    });
+    galleryBtn.addEventListener('click', () => {
+        noticeBtn.classList.remove('on');
+        galleryBtn.classList.remove('on');
+        noticeCont.classList.remove('on');
+        galleryCont.classList.remove('on');
+        galleryBtn.classList.add('on');
+        galleryCont.classList.add('on');
+    });
 };
