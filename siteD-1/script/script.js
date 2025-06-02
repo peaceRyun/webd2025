58e79a2df3752563a433cb74c491a11864a51317
peaceRyun <dbsrn1110@gmail.com>
2025-06-02 18:02:58 +0900

    10

---
+++ siteD-1/script/script.js
@@ -0,0 +1,63 @@
+window.onload = () => {
+    // nav
+    const navList = document.querySelectorAll('#gnb > ul > li');
+    navList.forEach((navItem) => {
+        navItem.addEventListener('mouseover', function () {
+            this.querySelector('.submenu').style.height = '130px';
+        });
+        navItem.addEventListener('mouseout', function () {
+            this.querySelector('.submenu').style.height = '0';
+        });
+    });
+    // sliders
+    let currentIndex = 0;
+    const sliderWrap = document.querySelector('.slider_wrap');
+    const slider = document.querySelectorAll('.slider_wrap > .slider');
+    const firstClone = sliderWrap.firstElementChild.cloneNode(true);
+    sliderWrap.appendChild(firstClone);
+
+    setInterval(() => {
+        currentIndex++;
+        sliderWrap.style.transition = 'all 0.6s';
+        sliderWrap.style.marginTop = `${-400 * currentIndex}px`;
+        if (currentIndex === slider.length) {
+            setTimeout(() => {
+                currentIndex = 0;
+                sliderWrap.style.transition = 'all 0s';
+                sliderWrap.style.marginTop = '0';
+            }, 700);
+        }
+    }, 3000);
+
+    // tab
+    const tabs = document.querySelectorAll('.tabs > button');
+    const tabCont = document.querySelectorAll('.tab_cont > ul');
+
+    tabs[0].classList.add('on');
+    tabCont[0].classList.add('on');
+
+    tabs.forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+            tabs.forEach((tab) => {
+                tab.classList.remove('on');
+            });
+            tabCont.forEach((tab) => {
+                tab.classList.remove('on');
+            });
+            tabs[index].classList.add('on');
+            tabCont[index].classList.add('on');
+        });
+    });
+
+    // pop
+    const popOpen = document.querySelector('.pop_open');
+    const popClose = document.querySelector('.pop_close');
+
+    popOpen.addEventListener('click', () => {
+        document.querySelector('.pop_wrap').classList.add('on');
+    });
+
+    popClose.addEventListener('click', () => {
+        document.querySelector('.pop_wrap').classList.remove('on');
+    });
+};
